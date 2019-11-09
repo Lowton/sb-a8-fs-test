2657137a08f77244090d83676f87705ff159e04a
Aleksey Kouzmenko <aleksey.kouzmenko@gmail.com>
2019-11-09 15:59:00 +0400

    Create simple Spring Boot application

---
+++ server/src/main/java/com/github/lowton/SpringBootRestApiApplication.java
@@ -0,0 +1,15 @@
+package com.github.lowton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+
+}
